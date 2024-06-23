54f7e89eddaedfc8c97e003f8837795c934859d6
Tobias Ves Losada <tveslosada@itba.edu.ar>
2024-06-23 20:07:03 -0300

    stats
    Co-Authored-By: Matias Ignacio Mutz <108437589+MatiasMutz@users.noreply.github.com>
    Co-Authored-By: Ben Deyheralde <108757682+bendeyhe@users.noreply.github.com>

---
+++ src/lib/headers/stats.h
@@ -0,0 +1,31 @@
+#ifndef STATS_H
+#define STATS_H
+
+#include <stdlib.h>
+
+typedef struct {
+       //suma de todas las conexiones desde siempre
+     size_t historicConnectionQuantity;
+
+     size_t currentConnectionQuantity;
+
+     //cantidad de bytes enviados por el servidor
+     size_t bytesSent;
+
+     //cantidad de bytes recibidos por el servidor
+     size_t bytesReceived;
+} TStats;
+
+    void statsInit();
+
+    void newUserConnection();
+
+    void userDisconnection();
+
+    void bytesSent(size_t bytes);
+
+    void bytesReceived(size_t bytes);
+
+    void getStats(TStats *stats);
+
+#endif
